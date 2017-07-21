727d7965a63b434429bef3a74a3b74c3eb836c1d
Cody <cody@hastings.email>
2017-07-21 16:12:20 -0500

    completed functions exercise in arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,9 @@
 <?php
 
+$a = 200000;
+
+$b = -86;
+
 function add($a, $b)
 {
     return $a + $b;
@@ -7,17 +11,34 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    // Add code here
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+	return $a % $b; 
 }
 
 // Add code to test your functions here
+
+$addResult = add(1, 2);
+
+$subResult = subtract(8, 2);
+
+$multiplyResult = multiply(8, 2);
+
+$divideResult = divide(10, 2);
+
+$modResult = modulus(8, 2);
+
+echo "add = $addResult, subtract = $subResult, multiply = $multiplyResult, divide = $divideResult, modulus = $modResult" . PHP_EOL;
