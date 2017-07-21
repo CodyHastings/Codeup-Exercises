5259e3e2f6f36a7132db3e8d8ee42a7dcd03052b
Cody <cody@hastings.email>
2017-07-21 12:14:32 -0500

    Completed foreach_book.php exercise

--- foreach_book.php
+++ foreach_book.php
@@ -22,3 +22,37 @@ $books = array(
         'pages' => 544
     )
 );
+
+//  :::::::  only show books after 1950
+// foreach($books as $book => $info) {
+// 	foreach($info as $key => $value) {
+// 		if ($value > 1950) {
+// 			echo "=============" .PHP_EOL;
+// 			echo "Title : {$book}" .PHP_EOL;
+// 			echo "$key : $value" .PHP_EOL;
+// 		}
+// 	}
+// }
+// bonus 1 :::::::: only show books with pages less than 300
+// foreach($books as $book => $info) {
+// 	if ($info['pages'] < 300) {
+// 		echo "=============" .PHP_EOL;
+// 		echo "Title : {$book}" .PHP_EOL;
+// 		foreach($info as $key => $value) {
+// 			echo "$key : $value" .PHP_EOL;
+// 		}
+// 	}
+// }
+$numberOfPages = [];
+
+foreach($books as $book => $info) {
+	array_push($numberOfPages, $info['pages']) .PHP_EOL;
+}
+foreach($numberOfPages as $pages){
+
+	echo $pages .PHP_EOL;
+}
+
+	
+// }
+
