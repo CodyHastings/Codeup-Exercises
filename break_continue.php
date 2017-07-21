b21d0c078a3a196e8787d3bf99cd9261a2ff128c
Cody <cody@hastings.email>
2017-07-21 12:20:35 -0500

    completed break_continue.php exercise

--- break_continue.php
+++ break_continue.php
@@ -1,1 +1,21 @@
 <?php
+
+// Create a file named break_continue.php in your exercises directory. Commit and push to GitHub after each step.
+
+// Create a for loop that shows all even numbers between 1 and 100 using continue.
+
+// Create another for loop that counts from 1 to 100, but stops after 10 using break.
+
+// for ($i = 1; $i < 100; $i++) {
+// 	if ($i % 2 != 0) {
+// 		continue;
+// 	}
+// 	echo $i . PHP_EOL;
+// }
+
+for ($i = 1; $i < 100; $i++) {
+	if ($i > 10) {
+		break;
+	}
+	echo $i . PHP_EOL;
+}
