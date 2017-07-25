a6e2603cceced3b27b49906f569a2bd63434aae3
Cody <cody@hastings.email>
2017-07-25 10:25:07 -0500

    adding completed merge-arrays.php

--- merge-arrays.php
+++ merge-arrays.php
@@ -12,29 +12,70 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-function combine_arrays($names, $compare) 
+function isInArray($needle, $haystack) 
 {
-	if (the arrays have the same value at the same index) {
-		// it should only be added once
-	} else {
-		// the value from the first array should be added and then the second. 
+    // if $needle exists anywhere in the array $haystack, return true.
+    // otherwise, return false.
+
+    // be CAREFUL b/c array_search returns the index and that index may be zero
+
+    if(array_search($needle, $haystack) === false) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+if(isInArray("Tina", $names)) {
+    echo "Tina is in the array" . PHP_EOL;
+} else {
+    echo "Tina aint in the array" . PHP_EOL;
+}
+
+if(isInArray("Bob", $names)) {
+    echo "Bob is in the array, babay and he's here to stay, yay!" . PHP_EOL;
+} else {
+    echo "Bob aint in the array." . PHP_EOL;
+}
+
+
+
+// Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+function numberOfValuesInCommon($array1, $array2)
+{
+    $count = 0;
+
+    // $value first time through the array is the string 'Tina'
+    foreach($array1 as $value) {
+        if(isInArray($value, $array2)) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+
+echo "The number of values that \$names and \$compare have in common is " . numberOfValuesInCommon($names, $compare) . PHP_EOL;
+
+function combine_arrays($array1, $array2) 
+{
+	$newArray = [];
+	foreach ($array1 as $key => $value) {
+		if (isInArray($value, $array2)) {
+			array_push($newArray, $array1[$key]);
+		} else {
+			array_push($newArray, $array1[$key]);
+			array_push($newArray, $array2[$key]);
+		}
+
 	}
-
-
 
 	return $newArray;
 }
 
-// out should match this
- // Array
- // (
- //     [0] => Tina
- //     [1] => Dana
- //     [2] => Dean
- //     [3] => Mike
- //     [4] => Mel
- //     [5] => Amy
- //     [6] => Adam
- //     [7] => Michael
- // )
+$newArray = combine_arrays($names, $compare);
+
+print_r($newArray);
+
 
