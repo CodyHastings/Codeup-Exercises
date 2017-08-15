1693c3521868dc0146e1fc264d78f4f43f6ed2fd
Cody <cody@hastings.email>
2017-08-15 15:22:22 -0500

    completed step 2 of exercise

--- Rectangle.php
+++ Rectangle.php
@@ -1,1 +1,29 @@
 <?php
+
+// In this new file, create a class named Rectangle that contains
+
+// the properties $height and $width
+// a constructor to set the height and width on instantiation
+// a method named area() that returns the area based on the height and width.
+
+class Rectangle 
+{
+	public $height;
+	public $width;
+
+	public function __construct($height, $width) 
+	{
+		$this->height = $height;
+		$this->width = $width;
+	}
+
+	public function area()
+	{
+
+		return $this->height * $this->width;
+
+	}
+
+
+
+}
