f67ca69fc58f3a7f2d2ac68b6976c3cd35fbd894
Cody <cody@hastings.email>
2017-08-16 09:00:46 -0500

    commiting minor changes to exercise

--- Square.php
+++ Square.php
@@ -1,1 +1,16 @@
 <?php
+
+require_once "Rectangle.php";
+
+class Square extends Rectangle
+{
+
+	public function perimeter()
+	{
+
+		return $this->area() * 2;
+
+	}
+	
+
+}
--- shapes_test.php
+++ shapes_test.php
@@ -2,8 +2,15 @@
 
 require_once "Rectangle.php";
 
+require_once "Square.php";
+
 
 $rect1 = new Rectangle(40,20);
 
 
 echo $rect1->area() . PHP_EOL;
+
+
+$square1 = new Square(40,40);
+
+echo $square1->perimeter();
